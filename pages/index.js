f42b1afdd40032d434429eed440dc24bb17cff8e
Dharamveer <dveersingh000@gmail.com>
2024-10-19 00:56:35 +0530

    task management app basic setup

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import Login from "./login/Login";
+import Register from "./register/Register";
+import Dashboard from "./dashboard/Dashboard";
+import NotFound from "./notfound/NotFound";
+
+export { Login, Register, Dashboard, NotFound };
