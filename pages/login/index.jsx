ea5f86c775a982a20610f23bf3232023bde52d12
Dharamveer <dveersingh000@gmail.com>
2024-10-22 03:47:48 +0530

    login page building and basic server setup

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,7 +1,93 @@
-import React from 'react'
+import React from "react";
+import Form from "../../components/form";
+import { useState } from "react";
+import styles from "./index.module.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const goToLogin = () => navigate("/dashboard");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState({
+    email: false,
+    password: false,
+  });
+
+  const formFields = [
+    
+    {
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      onchange: (e) => setFormData({ ...formData, email: e.target.value }),
+    },
+    {
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      onchange: (e) => setFormData({ ...formData, password: e.target.value }),
+    },
+  ];
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    console.log(errorMessage.name.isValid);
+    Object.keys(errorMessage).forEach((key) => {
+      if (!errorMessage[key].isValid) {
+        errorMessage[key].onError();
+      }
+    });
+  };
+  console.log(error);
+  const errorMessage = {
+    email: {
+      message: "Email is required",
+      isValid: formData.email.length > 0,
+      onError: () => {
+        setError((error) => ({ ...error, email: true }));
+      },
+    },
+    password: {
+      message: "Password is required",
+      isValid: formData.password.length > 0,
+      onError: () => {
+        setError((error) => ({ ...error, password: true }));
+      },
+    },
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className={styles.container}>
+        <div className={styles.leftContainer}>
+          <img src="../src/assets/Group.png" alt="logo" />
+          <p>
+            Welcome aboard my friend <br />
+            <span>just a couple of clicks and we start</span>
+          </p>
+        </div>
+        <div className={styles.rightContainer}>
+          <div className={styles.header}>Login</div>
+          <Form
+            error={error}
+            formFields={formFields}
+            onSubmit={onSubmit}
+            errorMessage={errorMessage}
+          />
+          <div className={styles.footer}>
+            <p>Have no account yet?</p>
+            <button 
+              type="button" 
+              onClick={goToLogin} 
+              className={styles.loginButton}
+            >Register</button>
+             
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
